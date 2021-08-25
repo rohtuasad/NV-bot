af3ba9b8193195ca7fc48bf28f355fc79575c1ae
Volkov Aleksei <rohtuasad@gmail.com>
2021-08-25 22:30:10 +0300

    Initialize application

---
+++ src/main/java/ru/rohtuasad/nvbot/NvBotApplication.java
@@ -0,0 +1,13 @@
+package ru.rohtuasad.nvbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NvBotApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NvBotApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ru/rohtuasad/nvbot/NvBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rohtuasad.nvbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NvBotApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
